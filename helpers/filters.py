207ccdab110f67dc89c41ec817dfe90c04e0b55b
Anshyadavji <104668308+Anshyadavji@users.noreply.github.com>
2022-05-02 08:26:53 +0530

    Update filters.py

--- helpers/filters.py
+++ helpers/filters.py
@@ -8,5 +8,5 @@ other_filters = filters.group & ~ filters.edited & ~ filters.via_bot & ~ filters
 other_filters2 = filters.private & ~ filters.edited & ~ filters.via_bot & ~ filters.forwarded
 
 
-def command(commands: Union[str, List[str]]):
+def command(commands: Union[str, List[str]]):c
     return filters.command(commands, COMMAND_PREFIXES)
